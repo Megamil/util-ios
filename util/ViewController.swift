7ccb081ecfffb09753f9db473732e5bb20412f02
Eduardo dos santos <rockero_spg@hotmail.com>
2018-01-13 11:37:06 -0200

    Initial Commit

---
+++ util/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  util
+//
+//  Created by Eduardo dos santos on 13/01/2018.
+//  Copyright © 2018 Eduardo dos santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
